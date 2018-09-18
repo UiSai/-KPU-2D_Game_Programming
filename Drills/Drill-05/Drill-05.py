977603d919aaf9c3931b0db132078dfc5bb36b3b
UiSai <ghju6780@outlook.kr>
2018-09-18 21:11:17 +0900

    두번째 함수 완성
    move_second_coordinates_to_third 함수를 완성했습니다.
    
    첫번째 함수(move_first_coordinates_to_second)에 주석을 추가했습니다. 나머지 함수에는 주석이 없습니다.
    fix_x, fix_y의 위치를 알기 쉽게 변경했습니다.
    좌표 주석을 삭제했습니다.

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -6,26 +6,13 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-"""거쳐야 할 좌표
-1 203, 535
-2 132 243
-3 535 470
-4 477 203
-5 715 136
-6 316 225
-7 510 92
-8 692 518
-9 682 336
-10 712 349
-"""
-
 def move_first_coordinates_to_second():
-    x, y = 203, 535
-    next_x, next_y = 132, 243
-    fix_x, fix_y = 203, 535
+    x, y = 203, 535  # 이동 중의 현재 좌표를 계산할 x, y값
+    fix_x, fix_y = 203, 535  # 기울기를 계산할 처음 위치의 고정 좌표값
+    next_x, next_y = 132, 243  # 목적지 좌표값
 
-    i = (next_y - y) / (next_x - x)
-    b = fix_y - i * fix_x
+    i = (next_y - y) / (next_x - x)  # 기울기 계산
+    b = fix_y - i * fix_x  # 상수 계산
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
@@ -36,7 +23,20 @@ def move_first_coordinates_to_second():
         delay(0.01)
 
 def move_second_coordinates_to_third():
-    pass
+    x, y = 132, 243
+    fix_x, fix_y = 132, 243
+    next_x, next_y = 535, 470
+
+    i = (next_y - y) / (next_x - x)
+    b = fix_y - i * fix_x
+
+    while (abs(x - next_x) > 0 ):
+        y = i * x + b
+
+        clear_canvas_now()
+        character.draw_now(x, y)
+        x += 1
+        delay(0.01)
 
 def move_third_coordinates_to_fourth():
     pass
