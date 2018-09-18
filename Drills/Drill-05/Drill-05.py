d33bad5e0fd727143b4d1a02d4f0b918c787e85a
UiSai <ghju6780@outlook.kr>
2018-09-18 18:40:11 +0900

    뼈대 완성
    함수 선언
    move_first(~tenth)_coordinates 선언.
    10개의 좌표로 순서대로 이동해야함.

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -4,7 +4,47 @@ open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
+def move_first_coordinates():
+    pass
+
+def move_second_coordinates():
+    pass
+
+def move_third_coordinates():
+    pass
+
+def move_fourth_coordinates():
+    pass
+
+def move_fifth_coordinates():
+    pass
+
+def move_sixth_coordinates():
+    pass
+
+def move_seventh_coordinates():
+    pass
+
+def move_eighth_coordinates():
+    pass
+
+def move_ninth_coordinates():
+    pass
+
+def move_tenth_coordinates():
+    pass
 
 
+while True:
+    move_first_coordinates()
+    move_second_coordinates()
+    move_third_coordinates()
+    move_fourth_coordinates()
+    move_fifth_coordinates()
+    move_sixth_coordinates()
+    move_seventh_coordinates()
+    move_eighth_coordinates()
+    move_ninth_coordinates()
+    move_tenth_coordinates()
 
 close_canvas()
