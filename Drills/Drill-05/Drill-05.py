0317da01e34d1fa860009099a6de7d23248fc37b
UiSai <ghju6780@outlook.kr>
2018-09-18 22:06:04 +0900

    애니메이션 추가
    이제 플리커링도 없고 애니메이션도 생겼어요! 모눈이 없는건 덤!
    
    https://youtu.be/gYEWGeIoEoc

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -2,25 +2,46 @@ from pico2d import *
 from math import *
 
 open_canvas()
+hide_lattice()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
+frame = 0
+
+def run_to_right(x, y):
+    global frame
+    right = 100
+
+    clear_canvas()
+    character.clip_draw(frame * 100, right, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+    get_events()
+
+def run_to_left(x, y):
+    global frame
+    left = 0
+
+    clear_canvas()
+    character.clip_draw(frame * 100, left, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+    get_events()
 
 def move_first_coordinates_to_second():
     x, y = 203, 535  # 이동 중의 현재 좌표를 계산할 x, y값
     fix_x, fix_y = 203, 535  # 기울기를 계산할 처음 위치의 고정 좌표값
     next_x, next_y = 132, 243  # 목적지 좌표값
 
-    i = (next_y - y) / (next_x - x)  # 기울기 계산
+    i = (next_y - y) / (next_x - x)  # 기울기 계산. 기울기 값에 따라 속도가 달라짐. 함수 마지막 부분의 x 증가량으로 속도 조절.
     b = fix_y - i * fix_x  # 상수 계산
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b  # 좌표값을 구함
-
-        clear_canvas_now()  # 캔버스 삭제
-        character.draw_now(x, y)  # 캔버스에 그림
+        run_to_left(x, y)
         x -= 1
-        delay(0.01)
 
 def move_second_coordinates_to_third():
     x, y = 132, 243
@@ -32,11 +53,8 @@ def move_second_coordinates_to_third():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_right(x, y)
         x += 1
-        delay(0.01)
 
 def move_third_coordinates_to_fourth():
     x, y = 535, 470
@@ -48,11 +66,8 @@ def move_third_coordinates_to_fourth():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_left(x, y)
         x -= 1
-        delay(0.01)
 
 def move_fourth_coordinates_to_fifth():
     x, y = 477, 203
@@ -64,11 +79,8 @@ def move_fourth_coordinates_to_fifth():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_right(x, y)
         x += 1
-        delay(0.01)
 
 def move_fifth_coordinates_to_sixth():
     x, y = 715, 136
@@ -80,11 +92,8 @@ def move_fifth_coordinates_to_sixth():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_left(x, y)
         x -= 1
-        delay(0.01)
 
 def move_sixth_coordinates_to_seventh():
     x, y = 316, 225
@@ -96,11 +105,8 @@ def move_sixth_coordinates_to_seventh():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_right(x, y)
         x += 1
-        delay(0.01)
 
 def move_seventh_coordinates_to_eighth():
     x, y = 510, 92
@@ -112,11 +118,8 @@ def move_seventh_coordinates_to_eighth():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_right(x, y)
         x += 1
-        delay(0.01)
 
 def move_eighth_coordinates_to_ninth():
     x, y = 692, 518
@@ -128,11 +131,8 @@ def move_eighth_coordinates_to_ninth():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_left(x, y)
         x -= 1
-        delay(0.01)
 
 def move_ninth_coordinates_to_tenth():
     x, y = 682, 336
@@ -144,11 +144,8 @@ def move_ninth_coordinates_to_tenth():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_right(x, y)
         x += 1
-        delay(0.01)
 
 def move_tenth_coordinates_to_first():
     x, y = 712, 349
@@ -160,13 +157,10 @@ def move_tenth_coordinates_to_first():
 
     while (abs(x - next_x) > 0 ):
         y = i * x + b
-
-        clear_canvas_now()
-        character.draw_now(x, y)
+        run_to_left(x, y)
         x -= 1
-        delay(0.01)
 
-
+delay(5)
 while True:
     move_first_coordinates_to_second()
     move_second_coordinates_to_third()
