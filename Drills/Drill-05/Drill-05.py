93fa7935f3069545691cc561b6380a4bcd073460
UiSai <ghju6780@outlook.kr>
2018-09-18 17:50:17 +0900

    함수 추가
    함수를 사용하기 위해 선언하였다.

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -5,12 +5,15 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-while (x < 800):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+def make_rectangle():
+    pass
+
+def make_circle():
+    pass
+
+
+while True:
+    make_rectangle()
+    make_circle()
 
 close_canvas()
