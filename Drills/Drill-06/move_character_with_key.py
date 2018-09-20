8c998370955645d1b048f1783db0497d9fadb664
UiSai <ghju6780@outlook.kr>
2018-09-20 18:14:33 +0900

    dir 변수 추가
    새로운 변수 dir을 통해 가속도를 넣었습니다.
    SDL_KEYUP을 통해 더 빠르게 움직입니다.

--- Drills/Drill-06/move_character_with_key.py
+++ Drills/Drill-06/move_character_with_key.py
@@ -4,6 +4,7 @@ from pico2d import *
 def handle_events():
     global running
     global x
+    global dir
 
     events = get_events()
 
@@ -12,11 +13,17 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_LEFT:
-                x -= 5
+                dir -= 1
             elif event.key == SDLK_RIGHT:
-                x += 5
+                dir += 1
             elif event.key == SDLK_ESCAPE:
                 running = False
+            elif event.type == SDL_KEYUP:
+                if event.key == SDLK_LEFT:
+                    dir -= 1
+                elif event.key == SDLK_RIGHT:
+                    dir += 1
+
 
 
 open_canvas()
@@ -26,15 +33,16 @@ character = load_image('animation_sheet.png')
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
     update_canvas()
-
     handle_events()
     frame = (frame + 1) % 8
+    x += dir * 5
     delay(0.05)
 
 close_canvas()
