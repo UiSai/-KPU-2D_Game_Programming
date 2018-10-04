81b64ddfecf1719c84ad61c41eaf347457396f8c
UiSai <ghju6780@outlook.kr>
2018-10-04 18:21:52 +0900

    두번째 커밋
    완료함

--- Drills/Drill-08/Drill-08(1).py
+++ Drills/Drill-08/Drill-08(1).py
@@ -82,7 +82,7 @@ def draw_curve_4_points(p1, p2, p3, p4):
     draw_point(p4)
 
     # draw p4-p1
-    for i in range(50, 150, 2):
+    for i in range(0, 100, 2):
         t = i / 100
         x = ((-t**3 + 2*t**2 - t)*p3[0] + (3*t**3 - 5*t**2 + 2) * p4[0] + (-3*t**3 + 4*t**2 + t) * p1[0] + (t**3 - t**2)*p2[0])/2
         y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2) * p4[1] + (-3*t**3 + 4*t**2 + t) * p1[1] + (t**3 - t**2)*p2[1])/2
@@ -93,8 +93,5 @@ def draw_curve_4_points(p1, p2, p3, p4):
 points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
 
 prepare_turtle_canvas()
-
-draw_curve_4_points(points[0], points[1], points[2], points[3])
-
 
 turtle.done()
