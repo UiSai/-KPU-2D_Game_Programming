a1cb5aa6b55adc5f59c1a2ab4798376ad0b6d647
UiSai <ghju6780@outlook.kr>
2018-10-11 17:10:55 +0900

    Drill-10(1) 완성
    2015180029 이다원 Drill-10(1) 커밋횟수 3회
    
    동영상 주소 : https://youtu.be/mM4Fy7mVy5k
    
    pause_state에 title_state의 내용을 복사하고 이미지를 변경함
    
    근데 되네?

---
+++ Drills/Drill-10/pause_state.py
@@ -0,0 +1,47 @@
+import game_framework
+from pico2d import *
+
+import title_state
+
+name = "PauseState"
+image = None
+logo_time = 0.0
+
+
+def enter():
+    global image
+    image = load_image('pausee.png')
+
+
+def exit():
+    global image
+    del(image)
+
+
+def update():
+    pass
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            game_framework.pop_state()
+
+
+def pause(): pass
+
+
+def resume(): pass
